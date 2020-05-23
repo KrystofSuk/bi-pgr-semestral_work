a1c97fdef44cad9db7fcf279f6a876efc84a84f6
Kryštof Šuk <suk.krystof@gmail.com>
2020-05-24 00:21:27 +0200

    Update loader.h

--- Tankz/Headers/Engine/Core/loader.h
+++ Tankz/Headers/Engine/Core/loader.h
@@ -9,9 +9,35 @@
 
 namespace sukkryst {
 
+	/**
+	 * @brief Config loader to load appdata, resources and scene json config files
+	 * 
+	 */
 	struct Loader {
+
+		/**
+		 * @brief Loading app data json
+		 * 
+		 * @param appPath path to appdata file
+		 * @param data object to fill info to
+		 */
 		static void LoadApp(const std::string& appPath, AppData& data);
+
+		/**
+		 * @brief Loading resources json
+		 * 
+		 * @param configPath path to resources file
+		 * @param resources object to fill info to
+		 */
 		static void LoadResources(const std::string& configPath, Resources& resources);
+
+		/**
+		 * @brief Loading scene json
+		 * 
+		 * @param scenePath path to scene file
+		 * @param scene object to fill info to
+		 * @param resources object to get info from like meshes and shaders
+		 */
 		static void LoadScene(const std::string& scenePath, Scene& scene, Resources& resources);
 	};
 
