f14ffc1d597076959fb0df1ac1fa112ea42190a0
Kryštof Šuk <suk.krystof@gmail.com>
2020-05-24 00:01:54 +0200

    Spline

--- Tankz/Headers/Engine/Core/spline.h
+++ Tankz/Headers/Engine/Core/spline.h
@@ -5,13 +5,33 @@
 
 namespace sukkryst {
 
+	/**
+	 * @brief Spline struct for catmull-rom 
+	 * 
+	 */
 	struct Spline
 	{
 		std::vector<glm::vec3> points;
 
+		/**
+		 * @brief Construct a new Spline object
+		 * 
+		 */
 		Spline();
+
+		/**
+		 * @brief Construct a new Spline object with defined points
+		 * 
+		 * @param points of the curve
+		 */
 		Spline(const std::vector<glm::vec3>& points);
 
+		/**
+		 * @brief Evaluates the animation in desired time
+		 * 
+		 * @param t desired time of animation
+		 * @return glm::vec3 result of catmull-rom
+		 */
 		glm::vec3 Evaluate(const float& t);
 	};
 
