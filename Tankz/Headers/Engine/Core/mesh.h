22ef868643c9f0b2b71fd2b14bc34977b86029fa
Kryštof Šuk <suk.krystof@gmail.com>
2020-05-24 00:17:17 +0200

    Update mesh.h

--- Tankz/Headers/Engine/Core/mesh.h
+++ Tankz/Headers/Engine/Core/mesh.h
@@ -7,10 +7,21 @@
 
 namespace sukkryst {
 
+	/**
+	 * @brief Mesh class representation with its contetns
+	 * 
+	 */
 	struct Mesh {
 		GLuint vao, vbo, ebo;
 		GLuint texture;
 		unsigned int faces, vert;
+
+		/**
+		 * @brief Construct a new Mesh object
+		 * 
+		 * @param fileName path to .obj
+		 * @param shader used shader to bind
+		 */
 		Mesh(const std::string& fileName, Shader* shader);
 	};
 
