257b8e9a24f6381ee1b89b0969e00f937030f1a0
Kryštof Šuk <suk.krystof@gmail.com>
2020-05-24 00:25:49 +0200

    Update game_object.h

--- Tankz/Headers/Engine/Core/game_object.h
+++ Tankz/Headers/Engine/Core/game_object.h
@@ -9,6 +9,10 @@
 
 namespace sukkryst {
 
+	/**
+	 * @brief Basic gameobject engine representation with update and on click methods + its containing components + transform
+	 * 
+	 */
 	class GameObject {
 	private:
 		std::unordered_map<std::string, Component*> _components;
@@ -17,17 +21,46 @@ namespace sukkryst {
 		std::string name;
 		unsigned char id;
 
+		/**
+		 * @brief Construct a new gameobject with specific name
+		 * 
+		 * @param name of gameobject
+		 */
 		GameObject(const std::string& name);
 
+		/**
+		 * @brief Calls update in every component
+		 * 
+		 */
 		void Update();
 
+		/**
+		 * @brief Adds new component to gameobject
+		 * 
+		 * @param component to add
+		 */
 		void AddComponent(Component* component);
 
+		/**
+		 * @brief Distribute click call to gameobject components if id mathes gameobject's id
+		 * 
+		 * @param char specific id of gameobject
+		 */
 		void OnClick(const unsigned char& id);
 
+		/**
+		 * @brief Get the Component casted to its type searched by desired type
+		 * 
+		 * @tparam T component type
+		 * @return T* founded component
+		 */
 		template <typename T>
 		T* GetComponent() const;
 
+		/**
+		 * @brief Destroy the gameobject
+		 * 
+		 */
 		~GameObject();
 	};
 
